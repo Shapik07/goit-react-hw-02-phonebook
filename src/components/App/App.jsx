9c3c7002fb9043fb5ceae2ad366f64b75ddecf70
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-10-29 12:23:13 +0300

    Update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,22 @@
 import { Component } from 'react';
 
 class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleName = e => {
+    this.setState({name: e.currentTarget.value})
+  }
+
   render() {
     return (
-      <p>Hello</p>
+      <form>
+        <label>
+          Name <input type="text" value={this.state.name} onChange={this.handleName} />
+        </label>
+      </form>
     )
   }
 }
